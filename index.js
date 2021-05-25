57b18c49f0969cd18c47d82c1f6480a5e668a5a0
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-25 14:02:38 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(record) {
+    function containsWin(element, index, array){
+        return (element.result === 'W')
+    }
+    let superbowlWin = record.find(containsWin)
+        console.log(superbowlWin)
 
+if (typeof superbowlWin === "object") {
+    return superbowlWin.year
+} else {
+    return undefined
+    }
+}
